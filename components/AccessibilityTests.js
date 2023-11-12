dda5fc35bc81233a207721d30f78afd063160d20
Jan Williams <jwilliams@tpgi.com>
2023-11-11 21:24:28 -0500

    Added tests to Prop Tests

--- components/AccessibilityTests.js
+++ components/AccessibilityTests.js
@@ -1,37 +1,67 @@
-import { StyleSheet, View, Text, Image, Platform } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Platform,
+  ScrollView,
+} from "react-native";
 import { Divider } from "@rneui/themed";
 function MiscPropTests() {
   return (
-    <View style={{ margin: 20 }}>
-      <View>
-        <Text style={styles.pText}>
-          Image with alt text via{" "}
-          <Text style={styles.code}>accessibilityLabel</Text> prop
-        </Text>
-        <Image
-          accessible={true}
-          accessibilityLabel="cupcake"
-          source={require("../assets/cupcake.jpg")}
-          style={{ width: 200, height: 200, margin: 10 }}
-        />
-      </View>
-      <Divider />
-      <View>
-        <Text role="heading" style={styles.headingText}>
-          Heading text with <Text style={styles.code}>role="heading"</Text> prop
-        </Text>
+    <ScrollView>
+      <View style={{ margin: 20 }}>
+        <View>
+          <Text style={styles.pText}>
+            Image with alt text via{" "}
+            <Text style={styles.code}>accessibilityLabel</Text> prop
+          </Text>
+          <Image
+            accessible={true}
+            accessibilityLabel="cupcake"
+            source={require("../assets/cupcake.jpg")}
+            style={{ width: 200, height: 200, margin: 10 }}
+          />
+        </View>
+        <Divider />
+        <View>
+          <Text role="heading" style={styles.headingText}>
+            Heading text with <Text style={styles.code}>role="heading"</Text>{" "}
+            prop
+          </Text>
 
-        <Text role="heading" style={styles.headingText}>
-          Heading text with{" "}
-          <Text style={styles.code}>accessibilityRole="header"</Text> prop
-        </Text>
-        <Text style={styles.pText} accessibilityElementsHidden="true">
-          This text is hidden from screen readers with the{" "}
-          <Text style={styles.code}>accessibilityElementsHidden="true"</Text>{" "}
-          prop
-        </Text>
+          <Text role="heading" style={styles.headingText}>
+            Heading text with{" "}
+            <Text style={styles.code}>accessibilityRole="header"</Text> prop
+          </Text>
+          <Text style={styles.pText} accessibilityElementsHidden={true}>
+            This text is hidden from iOS VoiceOver with the{" "}
+            <Text style={styles.code}>
+              accessibilityElementsHidden={"{true}"}
+            </Text>{" "}
+            prop
+          </Text>
+          <Text
+            style={styles.pText}
+            importantForAccessibility="no-hide-descendants"
+          >
+            This text is hidden from Android TalkBack with the
+            <Text style={styles.code}>
+              {" "}
+              importantForAccessibility="no-hide-descendants"
+            </Text>{" "}
+            prop
+          </Text>
+          <View aria-hidden={true}>
+            <Text style={styles.pText}>
+              This text is hidden from VoiceOver and TalkBack with{" "}
+              <Text style={styles.code}>aria-hidden={"{true}"}</Text> prop on
+              its parent <Text style={styles.code}>{"<View>"}</Text>
+            </Text>
+          </View>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 export default MiscPropTests;
